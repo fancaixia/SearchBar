deeff60f503ef1ceb6bca47e5f582257f581cca9
fancaixia <1562045201@qq.com>
2019-01-20 16:33:57 +0800

    SearchBar

---
+++ components/searchBox/index.js
@@ -0,0 +1,79 @@
+
+  // 本组件为搜索组件
+  // 需要传入addflag   值为true / false （是否显示搜索框右侧部分）
+  // 若显示搜索框右侧部分，addflag值为true  需传入右侧图标url以及addhandle函数
+
+Component({
+
+  properties: {
+
+    addflag: {    //显示搜索框右侧部分
+      type: Boolean,
+      value: false,   //如果不传addflg，那么默认不显示
+      observer(newVal, oldVal, changedPath) {
+
+      }
+    },
+    addimg: {       //显示搜索框右侧部分icon
+      type:String,
+      value:''
+    },
+    searchstr:{     //input  值
+      type:String,
+      value:'值'
+    },
+
+    },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    searchflag: false,   //搜索框 取消按钮显示与隐藏
+  },
+
+  /**
+   * 组件的方法列表
+
+   */
+  methods: {
+    
+    //获得焦点
+    getfocus(){
+      this.setData({
+        searchflag: true,
+      })
+    },
+    //搜索框右侧按钮事件
+    addhandle() {
+      this.triggerEvent("addhandle");
+    },
+    //搜索输入
+    searchList(e) {
+      this.triggerEvent("searchList",e);
+    },
+    //查询
+    endsearchList(e) {
+      
+      this.triggerEvent("endsearchList");
+    },
+    //失去焦点
+    blursearch(){
+      // console.log('失去焦点')
+
+    },
+    // 取消
+    cancelsearch() {
+      this.setData({
+        searchflag: false,
+      })
+      this.triggerEvent("cancelsearch");
+    },
+    //清空搜索框
+    activity_clear(e) {
+
+      this.triggerEvent("activity_clear");
+    },
+
+  }
+})
---
+++ pages/activity_list/index.js
@@ -0,0 +1,51 @@
+Page({
+  data: {
+    addflag:true,  //判断是否显示搜索框右侧部分
+    addimg:'../../images/activity_add.png',
+    searchstr:'',
+  },
+  onLoad(){
+
+   
+
+  },
+  onShow(){
+   
+  },
+
+  tap(e) {
+
+  },
+ 
+  // 搜索框右侧 事件
+  addhandle() {
+    console.log('触发搜索框右侧事件')
+  },
+
+  //搜索框输入时触发
+  searchList(ev) {
+    let e = ev.detail;
+    this.setData({
+      searchstr: e.detail.value
+    })
+  },
+  //搜索回调
+  endsearchList(e) {
+    console.log('查询数据')
+  },
+  // 取消搜索
+  cancelsearch() {
+    this.setData({
+      searchstr: ''
+    })
+  },
+  //清空搜索框
+  activity_clear(e) {
+
+    this.setData({
+      searchstr: ''
+    })
+  },
+
+
+})
